08b2d10a43450551843b2d94bd234816e5864fa3
gunadermawan <gunadermawan.official@gmail.com>
2023-09-15 22:37:19 +0700

    create BannerVertical

--- app/src/main/java/com/gunder/market/MainActivity.kt
+++ app/src/main/java/com/gunder/market/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.gunder.market.component.MainBannerVertical
 import com.gunder.market.component.MainBottomCategory
 import com.gunder.market.component.MainCardCategory
 import com.gunder.market.component.MainImageCategory
@@ -23,6 +24,7 @@ import com.gunder.market.component.MainTopCategory
 import com.gunder.market.component.TopMenu
 import com.gunder.market.model.dummyListBanner
 import com.gunder.market.model.dummyListBottomCategory
+import com.gunder.market.model.dummyListCardForYou
 import com.gunder.market.model.dummyListTopCategory
 import com.gunder.market.model.dummyListTopMenus
 import com.gunder.market.ui.theme.MarketTheme
@@ -57,6 +59,7 @@ fun MarketApp(modifier: Modifier = Modifier) {
         MainCategoryCard()
         MainCategoryBottom()
         MainImageCategory()
+        MainVerticalBanner()
     }
 }
 
@@ -75,6 +78,15 @@ fun MainCategoryTop() {
     LazyRow {
         items(dummyListTopCategory) {
             MainTopCategory(listTopCategory = it)
+        }
+    }
+}
+
+@Composable
+fun MainVerticalBanner() {
+    LazyRow {
+        items(dummyListCardForYou) {
+            MainBannerVertical(listBanner = it)
         }
     }
 }
